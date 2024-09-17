86bb6b15b0d5ef41582f8f2bf41683d6b8d36cad
ViniciusBhe <133768396+ViniciusBhe@users.noreply.github.com>
2024-09-17 11:25:28 -0300

    Add files via upload

---
+++ function cadastro_cliente(){.js
@@ -0,0 +1,6 @@
+function cadastro_cliente(){
+    const no_cl = document.getElementById('nome').value
+    const e_mail = document.getElementById('email').value
+    const tel_fone = document.getElementById('telefone').value
+    const cp_f = document.getElementById('CPF').value
+}
