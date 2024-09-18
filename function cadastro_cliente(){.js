392cba7903fa62eb111b5a5cdffdf9874a909e6e
di-milgral <128874321+di-milgral@users.noreply.github.com>
2024-09-18 08:31:06 -0300

    atualicao18/09

--- function cadastro_cliente(){.js
+++ function cadastro_cliente(){.js
@@ -3,4 +3,6 @@ function cadastro_cliente(){
     const e_mail = document.getElementById('email').value
     const tel_fone = document.getElementById('telefone').value
     const cp_f = document.getElementById('CPF').value
+
+    document.getElementById(cliente_cadastro).innerText=no_cl
 }
