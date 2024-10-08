3a0b662a6edec0c491efb988470b0f8fa498459a
di-milgral <128874321+di-milgral@users.noreply.github.com>
2024-10-08 11:30:49 -0300

    Add files via upload

---
+++ dump.sqlite.sql
@@ -0,0 +1,16 @@
+-- TABLE
+CREATE TABLE cliente(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  nome text NOT NULL,
+  sexo char NOT NULL,
+  celular text NOT NULL,
+  email text NOT NULL
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
