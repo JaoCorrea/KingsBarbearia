f881a1d920a7b88be42a16c965e07240bf8b57a5
JaoCorrea <128312734+JaoCorrea@users.noreply.github.com>
2024-10-09 08:43:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+function cadastrar(){
+    const nome = document.getElementById('nome').value; //pega o valor escrito no input
+    document.getElementById("nome_h").innerText = nome; //escreve no html
+
+    const cpf = document.getElementById('cpf').value; //pega o valor escrito no input
+    document.getElementById("cpf_h").innerText = cpf; //escreve no html
+
+    const fone = document.getElementById('fone').value; //pega o valor escrito no input
+    document.getElementById("fone_h").innerText = fone; //escreve no html
+
+    const fone = document.getElementById('fone').value; //pega o valor escrito no input
+    document.getElementById("fone_h").innerText = fone; //escreve no html
+
+}
+function agendar(){
+    const servico = document.getElementById('servico').value; //pega o valor escrito no input
+    document.getElementById("servico_h").innerText = servi//pega o valor escrito no input
+    e; //escreve no html
+
+    const hora = document.getElementById('hora').value; //pega o valor escrito no inputEndereçoEndereço
+    document.getElementById("hora_h").innerText = hora; //escreve no htmlEndereçomail
+
+    const prof = document.getElementById('prof').value; //pega o valor escrito no input
+    document.getElementById("prof_h").innerText = prof; //escreve no html
+
+}
+
+// profissionais
+function cadastrar_prof(){
+    const nome_prof = document.getElementById('nome_prof').value; //pega o valor escrito no input
+
+    const cpf_prof = document.getElementById('cpf_prof').value; //pega o valor escrito nomail input
+
+    const fone_prof = document.getElementById('fone_prof').value; //pega o valor escrito no input
+
+    const date_prof = document.getElementById('date_prof').value; //pega o valor escrito no input
+
+    const end_prof = document.getElementById('end_prof').value; //pega o valor escrito no input
+
+    const rg_prof = document.getElementById('rg_prof').value; //pega o valor escrito no input
+
+    const email_prof = document.getElementById('email_prof').value; //pega o valor escrito no input
+
+}
+// serviço
+function servico(){
+    const servico = document.getElementById('servico').value; //pega o valor escrito no input
+
+    const serv_valor = document.getElementById('serv_valor').value; //pega o valor escrito no input
+}
+
+
+// Fornecedor
+
+function servico(){
+    const forne = document.getElementById('forne').value; //pega o valor escrito no input
+
+    const forne_email = document.getElementById('forne_email').value; //pega o valor escrito no input
+
+    const forne_cnpj = document.getElementById('forne_cnpj').value; //pega o valor escrito no input
+
+    const forne_end = document.getElementById('forne_end').value;  //pega o valor escrito no input
+
+
+   
+}
